5edae2954e7ab3facdc468a41c78dadf7fb5d833
stephenhu3 <stephenhu3@gmail.com>
2015-12-26 16:58:56 -0800

    postgresql setup

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-### Website Word Counter
+## Website Word Counter
 
-## Powered by Flask, PostgreSQL, and SQLAchlemy
-## Front-end built with React and r-dom wrapper
+### Powered by Flask, PostgreSQL, and SQLAchlemy
+### Front-end built with React and r-dom wrapper
 
 - Automatic deploys from release branch
 - Development environment on dev branch
--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Result
 
 
 @app.route('/')
@@ -13,6 +18,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
